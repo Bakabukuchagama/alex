f8e9e645cedc31eb0b510a7f277dc057db2de549
A.Bragin <a.bragin@x5.ru>
2022-03-05 17:03:28 +0300

    init commit

---
+++ src/main/java/ru/test/alex/AlexApp.java
@@ -0,0 +1,14 @@
+package ru.test.alex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AlexApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AlexApp.class, args);
+  }
+
+}
---
+++ src/main/java/ru/test/alex/telegram/Bot.java
@@ -0,0 +1,43 @@
+package ru.test.alex.telegram;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+@Component
+@RequiredArgsConstructor
+public class Bot extends TelegramLongPollingBot {
+
+    private String name = "";
+    private String token = "";
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        send(new SendMessage(update.getMessage().getChatId().toString(), "hello"));
+    }
+
+    private void send(BotApiMethod<Message> method) {
+        try {
+            execute(method);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return name;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+}
